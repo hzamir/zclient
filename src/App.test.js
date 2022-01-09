c30fc9921b3ac513e99cc09d314a459cd722f825
Haim Zamir <haim@prometheusalts.com>
2022-01-08 20:22:30 -0500

    add todo to broken App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  // todo this test is broken until is the useSelector hook is step to work with it also
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
