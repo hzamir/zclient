a5feb9e4aba6c281b3d5720c10581c011a0ac677
Haim Zamir <haim@prometheusalts.com>
2022-01-19 13:53:02 -0500

    middleware refactor

--- src/actions/oms-middleware.js
+++ src/actions/oms-middleware.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {reqIdGenerate, elapsedSinceReqId} from "../utils/reqIdGenerator";
+import {firstArgs} from '../utils/first-args';
 
 const middlestyle = `
     padding: 2px 8px;
@@ -44,6 +45,46 @@ const axiosConfig = { timeout: 1000 };
 
 const sliceName = 'oms';
 
+// given an an api triggering action, asssign a reqId, and  name of its response and error actions
+const calcReqIdAndResponseTypes = aType => {
+
+    return {
+     reqId: reqIdGenerate(), // generate a unique request identifier
+     rAction: (aType + 'Response').slice(slicePrefixLen), // take off the prefix since we are exporting symbols in flat space no prefix
+     eAction: (aType + 'Error').slice(slicePrefixLen),
+    }
+};
+
+const response = (rAction, reqId, response) => {
+    // todo differentiate errorResponses (like non 200 status) and exceptions
+
+    const {elapsedMicros, elapsedStr:elapsed} = elapsedSinceReqId(performance.now(), reqId);
+
+    const respMeta = { reqId, elapsed, elapsedMicros };
+
+    try {
+        omsActions[rAction](response, respMeta);
+    } catch(err) {
+        console.error(`response action error`, rAction, omsActions);  // the issue is that the actions are not renamed when exported
+        throw new Error(`omsActions.${rAction}`);
+    }
+}
+
+const catchResponse =   (eAction, reqId,  error)=> {
+    const {elapsedMicros, elapsedStr:elapsed} = elapsedSinceReqId(performance.now(), reqId);
+    const {name,message,stack} = error;
+
+    // error information includes properties of error, reqId and elapsed time since request was made
+    const errorMeta = {name, message, reqId, elapsed, elapsedMicros, stack };
+
+    console.error(`${eAction} reqId:${reqId} exception after: ${elapsed}`, errorMeta);
+
+    // run either specific or generic api error handler
+
+    (omsActions[eAction] ?? omsActions['omsApiCatchAllError'])(errorMeta);
+};
+
+
 // todo better way to make middleware act only on its slice
 export const omsMiddleware = store => next => action => {
 
@@ -51,65 +92,34 @@ export const omsMiddleware = store => next => action => {
 
     const startsWithOms = aType.startsWith(`${sliceName}/`);
     const isResponse = startsWithOms && (aType.endsWith('Response') || aType.endsWith('Error'));  // all errors here are exceptions in this case
-    //
-    let reqId;
 
-    if(startsWithOms && !isResponse)
+    const intercept = startsWithOms && !isResponse;
+    if(!intercept)
+        return next(action);
+
+    const {reqId, rAction, eAction} = calcReqIdAndResponseTypes(aType);
+
+    const responsef = firstArgs(response, rAction, reqId);  // determine the reponse function to call
+    const catchf    = firstArgs(catchResponse, eAction, reqId);
+
+    const method = action.post? 'post': 'get';
+    const url = createUrl(action,method);
+
+    console.log(`%c ${aType} sends ${url}`, middlestyle);
+
+    switch(method)
     {
-        const reqId = reqIdGenerate(); // generate a unique request identifier
-
-        const rAction = (aType+'Response').slice(slicePrefixLen); // take off the prefix since we are exporting symbols in flat space no prefix
-        const eAction = (aType+'Error').slice(slicePrefixLen);
-
-        const responsef = (response)=> {
-
-            // todo differentiate errorResponses (like non 200 status) and exceptions
-
-            const {elapsedMicros, elapsedStr:elapsed} = elapsedSinceReqId(performance.now(), reqId);
-
-            const respMeta = { reqId, elapsed, elapsedMicros };
-
-            try {
-                omsActions[rAction](response, respMeta);
-            } catch(err) {
-                console.error(`response action error`, rAction, omsActions);  // the issue is that the actions are not renamed when exported
-                throw new Error(`omsActions.${rAction}`);
-            }
-
-        }
-        const catchf    = (error)=> {
-            const {elapsedMicros, elapsedStr:elapsed} = elapsedSinceReqId(performance.now(), reqId);
-            const {name,message,stack} = error;
-
-            // error information includes properties of error, reqId and elapsed time since request was made
-            const errorMeta = {name, message, reqId, elapsed, elapsedMicros, stack };
-
-            console.error(`${eAction} reqId:${reqId} exception after: ${elapsed}`, errorMeta);
-
-            // run either specific or generic api error handler
-
-            (omsActions[eAction] ?? omsActions['omsApiCatchAllError'])(errorMeta);
-        };
-
-        const method = action.post? 'post': 'get';
-        const url = createUrl(action,method);
-
-        console.log(`%c ${aType} sends ${url}`, middlestyle);
-
-        switch(method)
-        {
-            case 'post':
-                axios.post(url, {...action.body}, axiosConfig).then(responsef).catch(catchf);
-                break;
-            case 'get':
-            default:
-                if(action.params)
-                    axios.get(url, {...axiosConfig, params:action.params} ).then(responsef).catch(catchf);
-                else
-                    axios.get(url, axiosConfig).then(responsef).catch(catchf);
-        }
-        return next({...action,reqId, url}); // decorate action with reqId and url, actions will record it
+        case 'post':
+            axios.post(url, {...action.body}, axiosConfig).then(responsef).catch(catchf);
+            break;
+        case 'get':
+        default:
+            if(action.params)
+                axios.get(url, {...axiosConfig, params:action.params} ).then(responsef).catch(catchf);
+            else
+                axios.get(url, axiosConfig).then(responsef).catch(catchf);
     }
+    return next({...action,reqId, url}); // decorate action with reqId and url, actions will record it
 
-    return next(action);
+
 };
