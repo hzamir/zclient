7bf6b14f8cd359c24faf75773b1fa75c51d6e156
Haim Zamir <haim@prometheusalts.com>
2022-01-24 08:09:19 -0500

    ExpandIt border and stop event propagation

--- src/ExpandIt.tsx
+++ src/ExpandIt.tsx
@@ -1,17 +1,26 @@
-import React, {useState} from 'react';
+import React, {SyntheticEvent, useState} from 'react';
+import styled from 'styled-components';
+
+const MyDiv = styled.div`
+  border:           1px solid #060;
+`;
+
 
 interface ExpanditProps {
   title:string;
   children?: React.ReactNode;
 }
+
+
+
 export const Expandit = (props:ExpanditProps) => {
   const [expanded, setExpanded] = useState(false);
   const {title, children} = props;
     return (
-      <div onClick={()=>setExpanded(!expanded)}>
+      <MyDiv onClick={(e:SyntheticEvent)=>{setExpanded(!expanded); e.stopPropagation()}}>
       {title}<br/>
       {expanded && <div>{children}</div>}
-      </div>
+      </MyDiv>
     );
 
 }
