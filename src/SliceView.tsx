d7f3bd18a3ab99da2f3c053f6c4445d6135c6090
Haim Zamir <haim@prometheusalts.com>
2022-01-24 16:57:26 -0500

    changes to Horrible SliceView I will regret

--- src/SliceView.tsx
+++ src/SliceView.tsx
@@ -18,6 +18,7 @@ const MyTable = styled.table`
   
   width: 400px;
   border-collapse: collapse;
+  border: 1px solid blue;
   
   // alternate row colors
   tbody tr:nth-child(odd) {
@@ -39,26 +40,53 @@ const MyTable = styled.table`
 const stringize = (v:any) => JSON.stringify(v)
 
 interface RDivProps {
+  name:string|number;
   thing:any;
 }
 
 const RDiv = (props:RDivProps) => {
-  const {thing} = props;
+  const {thing, name} = props;
+
+  const thingy:any = thing as unknown as  any;
 
   if(thing && (typeof thing === 'object')) {
+
+    const len = Object.keys(thing).length;
     return (
-    <MyTable>
-      <thead>
-      <tr><th>Property</th><th>Value</th></tr>
-      </thead>
-      <tbody>
-      {/*list all the properties of auth slice in the dialog for debugging purposes*/}
-      {Object.entries(thing).map(([k,v])=><tr key={k}><td>{k}:</td><td>{stringize(v)}</td></tr>)}
-      </tbody>
-    </MyTable>);
-  } else {
-    return <span>{stringize(thing)}</span>
-  }
+      <Expandit title={name.toString()}>
+      <MyTable>
+        <tbody>
+        {/*list all the properties of slice in the dialog for debugging purposes*/}
+        {Object.entries(thing).map(([k, v]) => <tr key={k}>
+          <td>{k}:</td>
+          <td>{len? <RDiv name={k} thing={v}/>:<span>{stringize(thing)}</span>}</td>
+        </tr>)}
+        </tbody>
+      </MyTable>
+      </Expandit>);
+  } else if (thingy && Array.isArray(thingy)){
+    const len = thingy.length;
+    return (
+      <Expandit title={name.toString()}>
+      <MyTable>
+        <tbody>
+        {
+          thingy.map((v:any, i:number) =>
+            <tr key={i}>
+              <td>[{i}]:</td>
+              <td>{len? <RDiv name={i} thing={v}/>:
+                        <span>{stringize(thingy)}</span>}
+              </td>
+            </tr>
+            )
+        }
+        </tbody>
+      </MyTable>
+      </Expandit>
+      );
+   } else {
+        return <span>{stringize(thing)}</span>
+   }
 }
 
 
@@ -70,6 +98,7 @@ interface SliceViewProps {
 // todo get a list of its actions and parameters
 export const  SliceView = (props:SliceViewProps) => {
   const slice:Record<string, any> = useSelector((s:any)=>s[props.slice]);
+  const entries = Object.entries(slice);
   return (
       <SliceDiv>
         <Expandit title={`Slice '${props.slice}'`}>
@@ -79,7 +108,7 @@ export const  SliceView = (props:SliceViewProps) => {
           </thead>
           <tbody>
           {/*list all the properties of auth slice in the dialog for debugging purposes*/}
-          {Object.entries(slice).map(([k,v])=><tr key={k}><td>{k}:</td><td><RDiv thing={v}/></td></tr>)}
+          {entries.map(([k,v])=><tr key={k}><td>{k}:</td><td><RDiv name={k} thing={v}/></td></tr>)}
           </tbody>
         </MyTable>
         </Expandit>
