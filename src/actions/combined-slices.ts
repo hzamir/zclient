87b2ed8a9299fa9538105443f1ffcbf7488d7ad2
Haim Zamir <haim@prometheusalts.com>
2022-01-24 21:03:37 -0500

    fix TotalState to work with typed selector

--- src/actions/combined-slices.ts
+++ src/actions/combined-slices.ts
@@ -1,7 +1,7 @@
 // -- standard slices --
-import {sliceConfig as requestSlice} from "./request-slice";
-import {sliceConfig as notifySlice} from "./notify-slice";
-import {sliceConfig as coverageSlice} from './coverage-slice';
+import {sliceConfig as requestSlice,RequestState} from "./request-slice";
+import {sliceConfig as notifySlice,NotifyState} from "./notify-slice";
+import {sliceConfig as coverageSlice,CoverageState} from './coverage-slice';
 
 //-- standard middlewares
 import {loggingMiddleware} from "./logging-middleware";
@@ -9,10 +9,10 @@ import {fatalMiddleware} from './fatal-middleware';
 import {coverageMiddleware, coverageMiddlewareInit} from './coverage-middleware';
 
 // -- app specific slices --
-import {sliceConfig as localSlice} from "./local-slice";
-import {sliceConfig as controlSlice} from "./control-slice";
-import {sliceConfig as authSlice} from './auth-slice';
-import {sliceConfig as omsSlice}  from "./oms-slice";
+import {sliceConfig as localSlice, LocalState} from "./local-slice";
+import {sliceConfig as controlSlice, ControlState} from "./control-slice";
+import {sliceConfig as authSlice, AuthState} from './auth-slice';
+import {sliceConfig as omsSlice, OmsState}  from "./oms-slice";
 
 //-- app specific middlewares
 import {omsMiddleware, omsMiddlewareInit} from "./oms-middleware";
@@ -23,22 +23,17 @@ export const allSlices = [requestSlice, notifySlice, coverageSlice, omsSlice, lo
 export const allMiddlewares = [ fatalMiddleware, omsMiddleware, authMiddleware, coverageMiddleware, loggingMiddleware];
 export const middlewareInits = [ omsMiddlewareInit, authMiddlewareInit, coverageMiddlewareInit];
 
-// when I get smarter about deriving types in typescript I can fix this (he claims)
+// when I get smarter about deriving types in typescript I can presumably fix this (he claims)
 // but the important thing is it makes every part of state known
-export type TotalState = Readonly<{
-  // @ts-ignore
-  [requestSlice.name]: typeof requestSlice.initialState,
-  // @ts-ignore
-  [notifySlice.name]: typeof requestSlice.initialState,
-  // @ts-ignore
-  [coverageSlice.name]: typeof coverageSlice.initialState,
+//There is a source of truth problem, I need to derive the keys from the slice names directly encountered issues
+// with non-literals
+export type TotalState = {
+   request: RequestState;
+    notify: NotifyState;
+  coverage: CoverageState;
+       oms: OmsState;
+     local: LocalState;
+   control: ControlState;
+      auth: AuthState;
+}
 
-  // @ts-ignore
-  [omsSlice.name]: typeof omsSlice.initialState,
-  // @ts-ignore
-  [localSlice.name]: typeof localSlice.initialState,
-  // @ts-ignore
-  [controlSlice.name]: typeof controlSlice.initialState,
-  // @ts-ignore
-  [authSlice.name]: typeof authSlice.initialState,
-}>
